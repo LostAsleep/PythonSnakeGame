fb01f7bf71b009dd92105ed809e1830048518d64
LostAsleep <phammersen@protonmail.com>
2021-03-20 02:38:13 +0100

    Add a separate food class for the snake food.

---
+++ food.py
@@ -0,0 +1,18 @@
+import turtle
+import random
+
+
+class Food(turtle.Turtle):
+    """The snake food. Will be randomly placed on the screen upon init."""
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import turtle
 import time
 from snake import Snake
+from food import Food
 
 
 def main():
@@ -12,9 +13,8 @@ def main():
     screen.title("A Snake Game")
     screen.tracer(0)  # No automatic refreshing of the stuff on screen.
 
-
     snake = Snake()  # Create the snake using the Snake class.
-
+    food = Food()  # Create the initial snake food.
 
     screen.listen()  # Start listening for user input of specific keys.
     screen.onkey(snake.up, "Up")        # Remember: func without parens
@@ -22,17 +22,14 @@ def main():
     screen.onkey(snake.left, "Left")    # Remember: func without parens
     screen.onkey(snake.right, "Right")  # Remember: func without parens
 
-
     game_is_on = True
     while game_is_on:  # The main game loop.
         screen.update()  # Refresh the contents on screen.
         time.sleep(0.1)
         snake.move()
 
-
     screen.exitonclick()  # The window doesn't disappear directly after finishing.
 
 
 if __name__ == "__main__":
     main()
-
