afa4e2340ef34e80a46e300ad5b4ebeb36d4ea0e
LostAsleep <phammersen@protonmail.com>
2021-03-21 01:16:57 +0100

    Add collision detection for food and it's random relocation.

--- food.py
+++ food.py
@@ -6,6 +6,7 @@ class Food(turtle.Turtle):
     """The snake food. Will be randomly placed on the screen upon init."""
 
     def __init__(self):
+        """Initialize the food object to a random location."""
         super().__init__()
         self.shape("circle")
         self.penup()
@@ -16,3 +17,8 @@ class Food(turtle.Turtle):
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
 
+    def refresh(self):
+        """Move the food object to a random location."""
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from food import Food
 def main():
     """The main function of the snake game."""
 
-    screen = turtle.Screen()  # First create the game screen.
+    screen = turtle.Screen()             # First create the game screen.
     screen.setup(width=600, height=600)  # With it's dimensions.
-    screen.bgcolor("black")  # Set the background color.
+    screen.bgcolor("black")              # Set the background color.
     screen.title("A Snake Game")
     screen.tracer(0)  # No automatic refreshing of the stuff on screen.
 
     snake = Snake()  # Create the snake using the Snake class.
-    food = Food()  # Create the initial snake food.
+    food = Food()    # Create the initial snake food.
 
     screen.listen()  # Start listening for user input of specific keys.
     screen.onkey(snake.up, "Up")        # Remember: func without parens
@@ -23,12 +23,15 @@ def main():
     screen.onkey(snake.right, "Right")  # Remember: func without parens
 
     game_is_on = True
-    while game_is_on:  # The main game loop.
+    while game_is_on:    # The main game loop.
         screen.update()  # Refresh the contents on screen.
-        time.sleep(0.1)
-        snake.move()
+        time.sleep(0.1)  # The speed of the game.
+        snake.move()     # For continuous movement of the snake.
 
-    screen.exitonclick()  # The window doesn't disappear directly after finishing.
+        if snake.head.distance(food) < 15:  # Detect collision with food.
+            food.refresh()  # Move food to another random location.
+
+    screen.exitonclick()  # The window doesn't just disappear after looping.
 
 
 if __name__ == "__main__":
