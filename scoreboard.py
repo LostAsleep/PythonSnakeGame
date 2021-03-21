c6fc6b08207ee45b634511fba273b35d22b1d509
LostAsleep <phammersen@protonmail.com>
2021-03-21 19:53:28 +0100

    Delete unneccesary goto() and adds more comments.

--- scoreboard.py
+++ scoreboard.py
@@ -3,19 +3,19 @@ import turtle
 
 class Scoreboard(turtle.Turtle):
     """For displaying the current game score."""
+
     def __init__(self):
         """Initialize the scoreboard at the upper center of the game."""
         super().__init__()
         self.color("white")
-        self.hideturtle()
-        self.penup()
-        self.goto(0, 280)
+        self.hideturtle()  # No need to actually see the Turtle.
+        self.penup()       # No superfluous lines drawn.
+        self.goto(0, 280)  # Upper center.
         self.score = 0
 
     def show_score(self):
         """Print the current score. Clear text before updating"""
         current_score = f"Score: {self.score}"
-        self.goto(0, 280)
         self.clear()  # So that we get no overlaping text.
         self.write(arg=current_score, move=False, align="center")
 
