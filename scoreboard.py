87fc3730b0a3e61f40ba0229c9f96621251e75c7
LostAsleep <phammersen@protonmail.com>
2021-03-21 19:58:19 +0100

    Refactor via constants and do some text tweaking.

--- scoreboard.py
+++ scoreboard.py
@@ -1,6 +1,10 @@
 import turtle
 
 
+ALIGNEMENT = "center"
+FONT = ("Arial", 16, "normal")
+
+
 class Scoreboard(turtle.Turtle):
     """For displaying the current game score."""
 
@@ -10,14 +14,14 @@ class Scoreboard(turtle.Turtle):
         self.color("white")
         self.hideturtle()  # No need to actually see the Turtle.
         self.penup()       # No superfluous lines drawn.
-        self.goto(0, 280)  # Upper center.
+        self.goto(0, 275)  # Upper center.
         self.score = 0
 
     def show_score(self):
         """Print the current score. Clear text before updating"""
         current_score = f"Score: {self.score}"
         self.clear()  # So that we get no overlaping text.
-        self.write(arg=current_score, move=False, align="center")
+        self.write(arg=current_score, move=False, align=ALIGNEMENT, font=FONT)
 
     def increase_score(self):
         """Increases the score by one if called."""
