a4aa8e3926af7218a03ff273c98899e9d537d1a6
LostAsleep <phammersen@protonmail.com>
2021-04-07 21:16:55 +0200

    Add saving / loading of high score to a file.

--- scoreboard.py
+++ scoreboard.py
@@ -3,6 +3,7 @@ import turtle
 
 ALIGNEMENT = "center"
 FONT = ("Arial", 16, "normal")
+HIGHSCORE_FILE = "data.txt"
 
 
 class Scoreboard(turtle.Turtle):
@@ -16,7 +17,7 @@ class Scoreboard(turtle.Turtle):
         self.penup()       # No superfluous lines drawn.
         self.goto(0, 275)  # Upper center.
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.read_highscore()
         self.update()      # Print the intial score.
 
     def update(self):
@@ -34,11 +35,27 @@ class Scoreboard(turtle.Turtle):
         """Update the high score if neccessary and reset score."""
         if self.score > self.high_score:
             self.high_score = self.score
+            self.write_highscore()
         self.score = 0
         self.update()
 
-    # def game_over(self):
-    #     """Print the game over message."""
-    #     self.goto(0, 0)
-    #     self.color("red")
-    #     self.write(arg="GAME OVER", align=ALIGNEMENT, font=FONT)
+    def read_highscore(self):
+        """Read the old highscore from the data file."""
+        try:  # To read the file with the high scores.
+            with open(file=HIGHSCORE_FILE, mode="r") as file_handle:
+                contents = file_handle.read()
+                # Try to convert the file content into an integer.
+                # Return 0 if someting other than digits is in the file.
+                try:
+                    high_score = int(contents)
+                    return high_score
+                except ValueError:  
+                    return 0  
+        # Return 0 if no file with the name HIGHSCORE_FILE is found.
+        except FileNotFoundError:
+            return 0
+
+    def write_highscore(self):
+        """Write the new high score to the data file."""
+        with open(file=HIGHSCORE_FILE, mode="w") as file_handle:
+            file_handle.write(str(self.high_score))
